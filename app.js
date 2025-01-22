a6dd6b7cc4de11af2bc8552f8d5c09b2feb16887
Taykazuhiro <taykazuhiro@gmail.com>
2025-01-21 22:01:13 -0300

    função adicionarAmigo() pronta

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function limparCampo(){
+    novoAmigo = document.querySelector('#amigo');
+    novoAmigo.value = '';
+    document.getElementById('amigo').setAttribute('placeholder', 'Digite um nome');  
+    }
+function adicionarAmigo(){
+    const contemNumeros = /\d/;
+    let novoAmigo = document.querySelector('#amigo').value;
+    if (novoAmigo == '' || contemNumeros.test(novoAmigo)){
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(novoAmigo);
+        console.log(amigos.length);
+        limparCampo ();
+    }
+}
+
